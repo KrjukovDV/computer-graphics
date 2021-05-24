5291a0ba232c8469f7ad7d47bd2f067852b15a66
KrjukovDV <Den190401@yandex.ru>
2021-05-24 23:04:45 +0300

    ver2

---
+++ CG_lab3/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        int BasicProgramID;
+        int BasicVertexShader;
+        int BasicFragmentShader;
+        int vaoHandle;
+
+
+        private bool InitShaders()
+        {
+            string glVersion = GL.GetString(StringName.Version);
+            string glslVersion = GL.GetString(StringName.ShadingLanguageVersion);
+
+            BasicProgramID = GL.CreateProgram();
+            loadShader("..\\..\\raytracing.vert", ShaderType.VertexShader, BasicProgramID, out BasicVertexShader);
+            loadShader("..\\..\\raytracing.frag", ShaderType.FragmentShader, BasicProgramID, out BasicFragmentShader);
+            //Now that the shaders are added, the program needs to be linked.
+            //Like C code, the code is first compiled, then linked, so that it goes
+            //from human-readable code to the machine language needed.
+            GL.LinkProgram(BasicProgramID);
+            // Проверить успех компановки
+            int status = 0;
+            GL.GetProgram(BasicProgramID, GetProgramParameterName.LinkStatus, out status);
+            Console.WriteLine(GL.GetProgramInfoLog(BasicProgramID));
+            float[] positionData = { -1f, -1f, 0f, 1f, -1f, 0f, 1f, 1f, 0f, -1f, 1f, 0f };
+            float[] colorData = { 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f };
+            // Create and fill buffer objects
+            int[] vboHandlers = new int[2];
+            GL.GenBuffers(2, vboHandlers);
+            // Fill coordinate buffer
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vboHandlers[0]);
+            GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(sizeof(float) * positionData.Length), positionData, BufferUsageHint.StaticDraw);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vboHandlers[1]);
+            GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(sizeof(float) * colorData.Length), colorData, BufferUsageHint.StaticDraw);
+            // Create vertex object
+            vaoHandle = GL.GenVertexArray();
+            GL.BindVertexArray(vaoHandle);
+
+            GL.EnableVertexAttribArray(0);
+            GL.EnableVertexAttribArray(1);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vboHandlers[0]);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, 0);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vboHandlers[1]);
+            GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 0, 0);
+            return true;
+        }
+
+        void loadShader(String filename, ShaderType type, int program, out int address)
+        {
+            address = GL.CreateShader(type);
+            using (System.IO.StreamReader sr = new StreamReader(filename))
+            {
+                GL.ShaderSource(address, sr.ReadToEnd());
+            }
+            GL.CompileShader(address);
+            GL.AttachShader(program, address);
+            Console.WriteLine(GL.GetShaderInfoLog(address));
+        }
+
+        private static bool Init()
+        {
+            GL.Enable(EnableCap.ColorMaterial);
+            GL.ShadeModel(ShadingModel.Smooth);
+
+            GL.Enable(EnableCap.DepthTest);
+            GL.Enable(EnableCap.CullFace);
+
+            GL.Enable(EnableCap.Lighting);
+            GL.Enable(EnableCap.Light0);
+
+            GL.Hint(HintTarget.PerspectiveCorrectionHint, HintMode.Nicest);
+
+            return true;
+        }
+
+        private void Draw()
+        {
+            GL.ClearColor(Color.AliceBlue);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.LoadIdentity();
+            Matrix4 camera = Matrix4.LookAt(eye, target, up);
+            GL.LoadMatrix(ref camera);
+
+            GL.UseProgram(BasicProgramID);
+            GL.DrawArrays(PrimitiveType.Quads, 0, 4);
+            glControl1.SwapBuffers();
+            GL.UseProgram(0);
+
+        }
+
+        Vector3 eye = new Vector3(0, 0, 0);
+        Vector3 target = new Vector3(0, 0, -1);
+        Vector3 up = new Vector3(0, 1, 0);
+
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+
+            Init();
+            //Resize(openGlControl.Width, openGlControl.Height);
+            InitShaders();
+        }
+
+
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+
+            Draw();
+            // GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            ;
+
+            // Draw objects here
+
+           // glControl1.SwapBuffers();
+        }
+    }
+}
